713f424fdeb812bbf0a282bcedc30389c0dec4cc
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-09-23 07:14:42 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    printf("For Loop\n");
+
+    for(int i = 0; i <= 10; i=i+1){
+        printf("i value is : %d\n",i);
+    }
+    return 0;
+}
